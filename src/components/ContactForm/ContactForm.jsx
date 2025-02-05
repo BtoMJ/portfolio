b61419017523a46af6bff5e7d5729e8070e9da05
BtoMJ <bto.mena.17@gmail.com>
2025-02-05 00:58:35 -0600

    Funcionalidad de formulario de contacto

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,48 +1,80 @@
-import { useForm } from "react-hook-form";
+import { useRef, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
 import './ContactForm.css';
+// import { data } from "react-router-dom";
 
 const ContactForm = () => {
 
-    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    useEffect(()=>{
+        focus();
+    }, [])
 
-    const handleClearClick = () => {
-        reset();
-        const inputFocus = document.getElementById("name");
-        inputFocus.focus()
+    const form = useRef();
+
+    const notifyOk = () => {
+        toast.success("Datos enviados correctamente", {
+            autoClose: 2000,
+            hideProgressBar : false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: false,
+            progress: undefined,
+            theme: "light",
+            icon : true
+        });
+    };
+    
+    const focus = () => {
+        const inputName = document.getElementById("name");
+        inputName.value = "";
+        inputName.focus();
     }
 
-    const handleSubmitForm = (data) => {
-       console.log(data)
+    const cleanFocus = () => {
+        const inputEmail = document.getElementById("email");
+        inputEmail.value = "";
+        const Message = document.getElementById("message");
+        Message.value = "";
+        focus();
     }
 
-    console.log(errors)
+    const sendEmail = (e) => {
+
+        e.preventDefault();
+    
+        emailjs
+          .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, {
+                    publicKey: `${import.meta.env.VITE_PUBLIC_KEY}`,
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+              notifyOk();
+              cleanFocus();
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+    };
 
     return(
         <div className="contact-form-container">
-            
-            <form onSubmit={handleSubmit(handleSubmitForm)}>
-                <label id="name">
-                    Nombre
-                    <input {...register('name', { required:true })}  />
-                </label>
-                <br />
-                <br />
-                <label>
-                    Email
-                    <input {...register('email', { required:true })}  />
-                </label>
-                <br />
-                <br />
-                <label>
-                    Mensaje
-                    <textarea {...register('msg', { required:true })}  />
-                </label>
-                <br />
+
+            <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="name" id="name" required/>
+                <label>Email</label>
+                <input type="email" name="email" id="email" required/>
+                <label>Message</label>
+                <textarea name="message" id="message" required/>
                 <div className="btn-form-container">
-                    <button type="button" onClick={handleClearClick}>Limpiar</button>
+                    <button type="button" onClick={cleanFocus}>Limpiar</button>
                     <button type="submit">Enviar</button>
                 </div>
             </form>
+            <ToastContainer />
 
         </div>
     )
